246fd24e679d0d9d538f50ded902e7c4870bb3d8
kodri01 <kodrimukhtar5@gmail.com>
2026-03-26 20:59:55 +0700

    update v2.5

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -110,14 +110,14 @@ export const data = {
             icon: 'bx bxs-calendar-check',
             path: '#time',
         },
+        // {
+        //     id: 4,
+        //     teks: 'Galeri',
+        //     icon: 'bx bxs-photo-album',
+        //     path: '#galeri',
+        // },
         {
             id: 4,
-            teks: 'Galeri',
-            icon: 'bx bxs-photo-album',
-            path: '#galeri',
-        },
-        {
-            id: 5,
             teks: 'Ucapan',
             icon: 'bx bxs-message-rounded-dots',
             path: '#wishas',
